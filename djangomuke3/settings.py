4be4df93e7ae68a95e33702a2157ec079143acec
flyerooo <wxl.pure@gmail.com>
2017-06-06 23:22:35 +0800

    添加apps

--- djangomuke3/settings.py
+++ djangomuke3/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    # 'courses',
-    # 'organization',
-    # 'operation',
+    'courses',
+    'organization',
+    'operation',
     # 'xadmin',
     # 'crispy_forms',
 ]
